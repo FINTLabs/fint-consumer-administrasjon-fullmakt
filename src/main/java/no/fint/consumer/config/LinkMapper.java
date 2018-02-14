e273d227e4186d382577e08c9d92d6c528628ff7
Asgeir Nilsen <asgeir.nilsen@acando.no>
2018-02-14 14:00:09 +0100

    Added external links.

--- src/main/java/no/fint/consumer/config/LinkMapper.java
+++ src/main/java/no/fint/consumer/config/LinkMapper.java
@@ -1,10 +1,16 @@
 package no.fint.consumer.config;
 
-import no.fint.consumer.utils.RestEndpoints;
-import java.util.Map;
 import com.google.common.collect.ImmutableMap;
+import no.fint.consumer.utils.RestEndpoints;
 import no.fint.model.administrasjon.fullmakt.Fullmakt;
 import no.fint.model.administrasjon.fullmakt.Rolle;
+import no.fint.model.administrasjon.kodeverk.Ansvar;
+import no.fint.model.administrasjon.kodeverk.Art;
+import no.fint.model.administrasjon.kodeverk.Funksjon;
+import no.fint.model.administrasjon.kodeverk.Prosjekt;
+import no.fint.model.administrasjon.personal.Personalressurs;
+
+import java.util.Map;
 
 public class LinkMapper {
 
@@ -12,7 +18,11 @@ public class LinkMapper {
 		return ImmutableMap.<String,String>builder()
 			.put(Fullmakt.class.getName(), contextPath + RestEndpoints.FULLMAKT)
 			.put(Rolle.class.getName(), contextPath + RestEndpoints.ROLLE)
-			/* .put(TODO,TODO) */
+            .put(Personalressurs.class.getName(), "/administrasjon/personal/personalressurs")
+            .put(Ansvar.class.getName(), "/administrasjon/kodeverk/ansvar")
+            .put(Prosjekt.class.getName(), "/administrasjon/kodeverk/prosjekt")
+            .put(Art.class.getName(), "/administrasjon/kodeverk/art")
+            .put(Funksjon.class.getName(), "/administrasjon/kodeverk/funksjon")
 			.build();
 	}
 
