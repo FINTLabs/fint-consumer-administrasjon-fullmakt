bc76f95b3ff0980cf057b92bb865d3be59455636
Asgeir Nilsen <asgeir@fintlabs.no>
2019-01-25 21:38:52 +0100

    Prepare for production release.

--- src/main/java/no/fint/consumer/config/LinkMapper.java
+++ src/main/java/no/fint/consumer/config/LinkMapper.java
@@ -8,22 +8,24 @@ import no.fint.model.administrasjon.kodeverk.Ansvar;
 import no.fint.model.administrasjon.kodeverk.Art;
 import no.fint.model.administrasjon.kodeverk.Funksjon;
 import no.fint.model.administrasjon.kodeverk.Prosjekt;
+import no.fint.model.administrasjon.organisasjon.Organisasjonselement;
 import no.fint.model.administrasjon.personal.Personalressurs;
 
 import java.util.Map;
 
 public class LinkMapper {
 
-	public static Map<String, String> linkMapper(String contextPath) {
-		return ImmutableMap.<String,String>builder()
-			.put(Fullmakt.class.getName(), contextPath + RestEndpoints.FULLMAKT)
-			.put(Rolle.class.getName(), contextPath + RestEndpoints.ROLLE)
-            .put(Personalressurs.class.getName(), "/administrasjon/personal/personalressurs")
-            .put(Ansvar.class.getName(), "/administrasjon/kodeverk/ansvar")
-            .put(Prosjekt.class.getName(), "/administrasjon/kodeverk/prosjekt")
-            .put(Art.class.getName(), "/administrasjon/kodeverk/art")
-            .put(Funksjon.class.getName(), "/administrasjon/kodeverk/funksjon")
-			.build();
-	}
+    public static Map<String, String> linkMapper(String contextPath) {
+        return ImmutableMap.<String, String>builder()
+                .put(Fullmakt.class.getName(), contextPath + RestEndpoints.FULLMAKT)
+                .put(Rolle.class.getName(), contextPath + RestEndpoints.ROLLE)
+                .put(Personalressurs.class.getName(), "/administrasjon/personal/personalressurs")
+                .put(Ansvar.class.getName(), "/administrasjon/kodeverk/ansvar")
+                .put(Prosjekt.class.getName(), "/administrasjon/kodeverk/prosjekt")
+                .put(Art.class.getName(), "/administrasjon/kodeverk/art")
+                .put(Funksjon.class.getName(), "/administrasjon/kodeverk/funksjon")
+				.put(Organisasjonselement.class.getName(), "/administrasjon/organisasjon/organisasjonselement")
+                .build();
+    }
 
 }
