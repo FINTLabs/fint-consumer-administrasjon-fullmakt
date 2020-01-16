4f26dcfe8ee6c7fdef1abbf6f0766472e2a082fa
Asgeir Nilsen <asgeir@fintlabs.no>
2020-01-16 08:20:30 +0100

    Uptated to model 3.4.0
    Added compressing cache.

--- src/main/java/no/fint/consumer/config/LinkMapper.java
+++ src/main/java/no/fint/consumer/config/LinkMapper.java
@@ -4,10 +4,7 @@ import com.google.common.collect.ImmutableMap;
 import no.fint.consumer.utils.RestEndpoints;
 import no.fint.model.administrasjon.fullmakt.Fullmakt;
 import no.fint.model.administrasjon.fullmakt.Rolle;
-import no.fint.model.administrasjon.kodeverk.Ansvar;
-import no.fint.model.administrasjon.kodeverk.Art;
-import no.fint.model.administrasjon.kodeverk.Funksjon;
-import no.fint.model.administrasjon.kodeverk.Prosjekt;
+import no.fint.model.administrasjon.kodeverk.*;
 import no.fint.model.administrasjon.organisasjon.Organisasjonselement;
 import no.fint.model.administrasjon.personal.Personalressurs;
 
@@ -19,11 +16,20 @@ public class LinkMapper {
         return ImmutableMap.<String, String>builder()
                 .put(Fullmakt.class.getName(), contextPath + RestEndpoints.FULLMAKT)
                 .put(Rolle.class.getName(), contextPath + RestEndpoints.ROLLE)
-                .put(Personalressurs.class.getName(), "/administrasjon/personal/personalressurs")
+
+                .put(Aktivitet.class.getName(), "/administrasjon/kodeverk/aktivitet")
+                .put(Anlegg.class.getName(), "/administrasjon/kodeverk/anlegg")
                 .put(Ansvar.class.getName(), "/administrasjon/kodeverk/ansvar")
-                .put(Prosjekt.class.getName(), "/administrasjon/kodeverk/prosjekt")
                 .put(Art.class.getName(), "/administrasjon/kodeverk/art")
+                .put(Diverse.class.getName(), "/administrasjon/kodeverk/diverse")
                 .put(Funksjon.class.getName(), "/administrasjon/kodeverk/funksjon")
+                .put(Kontrakt.class.getName(), "/administrasjon/kodeverk/kontrakt")
+                .put(Lopenummer.class.getName(), "/administrasjon/kodeverk/lopenummer")
+                .put(Objekt.class.getName(), "/administrasjon/kodeverk/objekt")
+                .put(Prosjekt.class.getName(), "/administrasjon/kodeverk/prosjekt")
+                .put(Ramme.class.getName(), "/administrasjon/kodeverk/ramme")
+
+                .put(Personalressurs.class.getName(), "/administrasjon/personal/personalressurs")
 				.put(Organisasjonselement.class.getName(), "/administrasjon/organisasjon/organisasjonselement")
                 .build();
     }
