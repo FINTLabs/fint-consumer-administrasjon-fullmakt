213ef30ca366845e6ae4b0e8ae16d64098f37a28
Trond Magnus Sevre <trond@fintlabs.no>
2024-06-18 09:46:06 +0200

    Version 3.18.0.

--- src/main/java/no/fint/consumer/models/fullmakt/FullmaktCacheService.java
+++ src/main/java/no/fint/consumer/models/fullmakt/FullmaktCacheService.java
@@ -107,10 +107,7 @@ public class FullmaktCacheService extends CacheService<FullmaktResource> {
         } else {
             data = objectMapper.convertValue(event.getData(), javaType);
         }
-        data.forEach(resource -> {
-            linker.mapLinks(resource);
-            linker.resetSelfLinks(resource);
-        });
+        data.forEach(linker::mapLinks);
         if (FullmaktActions.valueOf(event.getAction()) == FullmaktActions.UPDATE_FULLMAKT) {
             if (event.getResponseStatus() == ResponseStatus.ACCEPTED || event.getResponseStatus() == ResponseStatus.CONFLICT) {
                 List<CacheObject<FullmaktResource>> cacheObjects = data
