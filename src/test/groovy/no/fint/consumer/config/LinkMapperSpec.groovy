de9ff7d80e119cfde45fd7842b11b78d1e9805e6
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2026-01-27 20:57:15 +0100

    add imports

--- src/test/groovy/no/fint/consumer/config/LinkMapperSpec.groovy
+++ src/test/groovy/no/fint/consumer/config/LinkMapperSpec.groovy
@@ -1,8 +1,16 @@
 package no.fint.consumer.config
 
+import no.novari.fint.model.administrasjon.fullmakt.Fullmakt
+import no.novari.fint.model.administrasjon.fullmakt.Rolle
+import no.novari.fint.model.administrasjon.kodeverk.Ansvar
+import no.novari.fint.model.administrasjon.kodeverk.Art
+import no.novari.fint.model.administrasjon.kodeverk.Funksjon
+import no.novari.fint.model.administrasjon.kodeverk.Prosjekt
+import no.novari.fint.model.administrasjon.organisasjon.Organisasjonselement
+import no.novari.fint.model.administrasjon.personal.Personalressurs
 import spock.lang.Specification
 
-class LinkMapperSpec extends Specification{
+class LinkMapperSpec extends Specification {
     def 'Assert that links to Kontodimensjon are present'() {
         given:
         def mapper = LinkMapper.linkMapper('/')
